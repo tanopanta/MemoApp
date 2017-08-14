1d47b4bbbb3ede54e18603be7fae32ef8be40e4a
tanopanta <rainstorm2525@gmail.com>
2017-08-14 23:12:17 +0900

    add voice input

--- app/src/main/java/com/example/tattata/memo/MainActivity.java
+++ app/src/main/java/com/example/tattata/memo/MainActivity.java
@@ -6,11 +6,15 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.os.storage.StorageManager;
 import android.os.storage.StorageVolume;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v4.provider.DocumentFile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,11 +27,14 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText;
@@ -35,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
 
     String fileName;
 
+    private static final int REQUEST_CODE_VOICE = 1000;
+    private static final int REQUEST_CODE_EXPORT = 1234;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button buttonSave = (Button)findViewById(R.id.buttonSave);
         Button buttonCancel = (Button)findViewById(R.id.buttonCancel);
+        ImageButton buttonVoice = (ImageButton)findViewById(R.id.imageButtonVoice);
 
         editText = (EditText)findViewById(R.id.editText);
         pref = getSharedPreferences("memoPref", MODE_PRIVATE);
@@ -61,6 +71,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                finishApp();
+            }
+        });
+        buttonVoice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "音声を入力");
+                startActivityForResult(intent, REQUEST_CODE_VOICE);
             }
         });
 
@@ -130,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
                 StorageVolume volume = sm.getPrimaryStorageVolume();
 
                 Intent intent = volume.createAccessIntent(Environment.DIRECTORY_DOWNLOADS);
-                startActivityForResult(intent, 1234);
+                startActivityForResult(intent, REQUEST_CODE_EXPORT);
             }
         });
         dialog.setNegativeButton("キャンセル", new DialogInterface.OnClickListener() {
@@ -143,29 +162,45 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
         if (resultCode == RESULT_CANCELED) {
             return;
         }
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-            return;
-        }
-        Uri uri = intent.getData();
-        getContentResolver().takePersistableUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        DocumentFile pickedDir = DocumentFile.fromTreeUri(this, uri);
+        switch (requestCode) {
+            case REQUEST_CODE_EXPORT:
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                    return;
+                }
+                Uri uri = intent.getData();
+                getContentResolver().takePersistableUriPermission(uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                DocumentFile pickedDir = DocumentFile.fromTreeUri(this, uri);
 
-        //text/plainとかだと勝手に拡張子を付けちゃう
-        DocumentFile newFile = pickedDir.createFile("text/spain", fileName);
-        try {
-            OutputStream out = getContentResolver().openOutputStream(newFile.getUri());
-            out.write(editText.getText().toString().getBytes());
-            out.close();
-            Toast.makeText(getApplicationContext(), fileName + "をダウンロードフォルダに保存しました。", Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            Toast.makeText(getApplicationContext(), "FileNotFoundException", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "IOException", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
+                //text/plainとかだと勝手に拡張子を付けちゃう
+                DocumentFile newFile = pickedDir.createFile("text/spain", fileName);
+                try {
+                    OutputStream out = getContentResolver().openOutputStream(newFile.getUri());
+                    out.write(editText.getText().toString().getBytes());
+                    out.close();
+                    Toast.makeText(getApplicationContext(), fileName + "をダウンロードフォルダに保存しました。", Toast.LENGTH_LONG).show();
+                } catch (FileNotFoundException e) {
+                    Toast.makeText(getApplicationContext(), "FileNotFoundException", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "IOException", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+                break;
+            case REQUEST_CODE_VOICE:
+                ArrayList<String> results = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if(results.size() > 0) {
+                    // 認識結果候補で一番有力なものを表示
+                    editText.getText().insert(editText.getSelectionEnd(), results.get(0));
+                }
+                break;
+            default:
+                break;
         }
+
+
     }
 }
